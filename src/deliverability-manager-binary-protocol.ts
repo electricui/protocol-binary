a2b4784e9e7457c5dd96f6a9b9db2cb71e074a94
Michael <kill.coder@gmail.com>
2019-02-07 21:51:59 +1030

    feat Share ackNum handling between the queue and the deliverability handler
    Closes #381

--- src/deliverability-manager-binary-protocol.ts
+++ src/deliverability-manager-binary-protocol.ts
@@ -1,4 +1,5 @@
 import { ConnectionInterface, DeliverabilityManager, Message } from '@electricui/core'
+import { MAX_ACK_NUM } from '@electricui/protocol-binary-constants'
 
 interface DeliverabilityManagerBinaryProtocolOptions {
   connectionInterface: ConnectionInterface
@@ -20,24 +21,55 @@ export default class DeliverabilityManagerBinaryProtocol extends DeliverabilityM
     // if there's no ack bit set, just send it blindly
     if (!message.metadata.ack) {
       return queryManager.push(message)
+    } else if (message.metadata.ackNum === 0) {
+      // If the ack bit is high and the ackNum is 0, set it to 1
+      // If the ack bit is high but the ackNum is not 0, leave the ackNum at whatever the queue set it to
+      message.metadata.ackNum = 1
     }
 
-    // mutate the ack bit and query bit
+    if (message.metadata.ackNum > MAX_ACK_NUM) {
+      console.warn(`A message got through with an ackNum > ${MAX_ACK_NUM}`)
+      console.trace()
+      return Promise.reject(
+        new Error(`Cannot send message with ackNum > ${MAX_ACK_NUM}`),
+      )
+    }
+
+    // mutate the query bit to reflect that we want an ack back
     message.metadata.query = true
-    message.metadata.ackNum = 2
 
     // copy the payload at this stage to re-inject later
     const copiedPayload = message.payload
 
     const connection = this.connectionInterface.getConnection()
 
+    // Create copies of the information we want in case they get mutated
+    const desiredMessageID = message.messageID
+    const desiredackNum = message.metadata.ackNum
+
     const { promise: waitForReply, cancel } = connection.waitForReply(
       (replyMessage: Message) => {
         // wait for a reply with the same ackNum and messageID
 
+        if (
+          replyMessage.messageID === desiredMessageID &&
+          replyMessage.metadata.ackNum !== desiredackNum
+        ) {
+          console.warn(
+            'got the wrong ackNum back, wtf? got',
+            replyMessage.metadata.ackNum,
+            'wanted',
+            desiredackNum,
+          )
+        }
+
         return (
-          replyMessage.messageID === message.messageID &&
-          replyMessage.metadata.ackNum === message.metadata.ackNum
+          // we want it to be the same messageID
+          replyMessage.messageID === desiredMessageID &&
+          // it shouldn't be a query, it's just a reply
+          replyMessage.metadata.query === false &&
+          // and the reply needs to match the expected ackNum
+          replyMessage.metadata.ackNum === desiredackNum
         )
       },
       this.timeout,
