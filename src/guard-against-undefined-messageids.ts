d9d09b709d752e833b43d852de039a44c2c3c754
Michael Orenstein <michael@whiteshadows.me>
2021-01-05 21:47:21 +1030

    refactor: By default only warn for undefined messageIDs

--- src/guard-against-undefined-messageids.ts
+++ src/guard-against-undefined-messageids.ts
@@ -1,15 +1,17 @@
 import { CancellationToken, DuplexPipeline, Message, Pipeline, TypeCache } from '@electricui/core'
 
 /**
- * This pipeline throws errors if a typecache doesn't contain type information for a messageID
+ * This pipeline throws errors or warns if a typecache doesn't contain type information for a messageID.
  */
 class UndefinedMessageIDGuardEncoderPipeline extends Pipeline {
   typeCache: TypeCache
   runtimeMessageIDs: string[]
-  constructor(typeCache: TypeCache, runtimeMessageIDs: string[]) {
+  severity: 'error' | 'warn'
+  constructor(typeCache: TypeCache, runtimeMessageIDs: string[], severity?: 'error' | 'warn') {
     super()
     this.typeCache = typeCache
     this.runtimeMessageIDs = runtimeMessageIDs
+    this.severity = severity ? severity : 'warn'
   }
 
   receive(message: Message, cancellationToken: CancellationToken) {
@@ -18,9 +20,19 @@ class UndefinedMessageIDGuardEncoderPipeline extends Pipeline {
       const cachedTypeData = this.typeCache.get(message.messageID)
 
       if (cachedTypeData === undefined && !this.runtimeMessageIDs.includes(message.messageID)) {
-        throw new Error(
-          `MessageID '${message.messageID}' does not have a type in the type cache. It has not been received from the hardware yet. Perhaps there is a typo in the messageID, or the handshake has not been run yet, a hot reload might have wiped the type cache requiring a re-handshake.`,
-        )
+        const consoleMessage = `MessageID '${message.messageID}' does not have a type in the type cache. It has not been received from the hardware yet. Perhaps there is a typo in the messageID, or the handshake has not been run yet, a hot reload might have wiped the type cache requiring a re-handshake.`
+
+        switch (this.severity) {
+          case 'warn':
+            console.warn(consoleMessage)
+            break
+          case 'error':
+            throw new Error(
+              `MessageID '${message.messageID}' does not have a type in the type cache. It has not been received from the hardware yet. Perhaps there is a typo in the messageID, or the handshake has not been run yet, a hot reload might have wiped the type cache requiring a re-handshake.`,
+            )
+          default:
+            break
+        }
       }
     }
 
