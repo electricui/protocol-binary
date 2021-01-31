fa64719c21603dff565747e4ccc8cd3ba9aadf42
Michael Orenstein <michael@whiteshadows.me>
2021-01-31 22:41:58 +1030

    chore: Updated benchmarks for protocol-binary

--- benchmarks/encode.js
+++ benchmarks/encode.js
@@ -1,59 +1,60 @@
-const encode = require('./../lib/index').encode
+const b = require('benny')
 
-suite('Binary Protocol Encoder', () => {
-  benchmark('Encoding 4 byte binary packet', () => {
-    const message = {
-      deviceID: null,
-      messageID: 'test',
-      payload: Buffer.from([0x00, 0x01, 0x02, 0x03]),
+const encode = require('./../lib/cjs/index').encode
 
-      // metadata defaults
-      metadata: {
-        type: 4,
-        internal: false,
-        query: false,
-        offset: null,
-        ack: false,
-        ackNum: 0,
-      },
+class Message {
+  isMessage = true
+
+  constructor(messageID, payload) {
+    this.deviceID = null
+    this.messageID = messageID
+    this.payload = payload
+
+    // metadata defaults
+    this.metadata = {
+      type: 0,
+      internal: false,
+      query: false,
+      offset: null,
+      ack: false,
+      ackNum: 0,
+      timestamp: 0,
     }
 
-    encode(message)
-  })
-  benchmark('Encoding 16 byte binary packet', () => {
-    const message = {
-      deviceID: null,
-      messageID: 'test',
-      payload: Buffer.from([
-        0x00,
-        0x01,
-        0x02,
-        0x03,
-        0x00,
-        0x01,
-        0x02,
-        0x03,
-        0x00,
-        0x01,
-        0x02,
-        0x03,
-        0x00,
-        0x01,
-        0x02,
-        0x03,
-      ]),
+    this.setPayload = this.setPayload.bind(this)
+  }
 
-      // metadata defaults
-      metadata: {
-        type: 4,
-        internal: false,
-        query: false,
-        offset: null,
-        ack: false,
-        ackNum: 0,
-      },
-    }
+  setPayload(payload) {
+    this.payload = payload
+
+    return this
+  }
+
+  /**
+   * Create (clone) a new message from an old message
+   * @param message Old message
+   */
+  static from(message) {
+    // We don't need to do any complicated cloning, if later on the payload is mutated that's fine,
+    // since the old message will still point at the original payload reference
+    const newMessage = new Message(message.messageID, message.payload)
+    newMessage.deviceID = message.deviceID
+    newMessage.metadata = Object.assign({}, message.metadata)
+
+    return newMessage
+  }
+}
+
+b.suite(
+  `Binary Protocol Encoder`,
+
+  b.add('Encoding 4 byte binary packet', () => {
+    const message = new Message('test', Buffer.from([0x00, 0x01, 0x02, 0x03]))
 
     encode(message)
-  })
-})
+  }),
+
+  b.cycle(),
+  b.complete(),
+  b.save({ file: `event-creation`, format: 'chart.html' }),
+)
